ebd5f7a89190cb7f786bbc2e948ca3669acf0ff2
Luis Munoz <lmunoz@cryptorepair.com>
2022-08-09 16:01:38 -0400

    feat: setting up post call

--- user_profile.php
+++ user_profile.php
@@ -11,6 +11,24 @@ switch ($method) {
         $row = pg_fetch_assoc($result);
         $output = $row;
         break;
+    case 'POST':
+        $raw=file_get_contents('php://input');
+        $data=json_decode($raw,true);
+        $public_address = $data['public_address'];
+        $crypto_dns = $data['crypto_dns'];
+        $privacy_mode = $data['privacy_mode'];
+        $user_email = $data['email'];
+        $user_phone_number = $data['phone_number'];
+        $user_country_of_residence = $data['country_of_residence'];
+        $user_full_name = $data['full_name'];
+        
+        $query = "INSERT INTO (public_address,crypto_dns,privacy_mode,user_email,user_phone_number,user_country_of_residence,user_full_name)  VALUES ({$public_address},{$crypto_dns},{$privacy_mode},{$user_email},{$user_phone_number},{$user_full_name})";
+        echo $query;
+        // $output = array(
+        //     'status_code' => 200,
+        //     'message' => 'User has been created.',
+        // );
+        break;
     default:
         $output = array(
             'status_code' => 500,
